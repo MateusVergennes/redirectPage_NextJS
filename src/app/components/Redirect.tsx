ddf52ed836bc4555bb64712846deb820cba9b754
MateusVergennes <mateusalpi@hotmail.com>
2023-12-02 01:56:30 -0300

    first commit

---
+++ src/app/components/Redirect.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Redirect = () => {
+  return (
+    <div>
+        <div className="text-center">
+        <h4 className="mb-3">Redirecionando</h4>
+        <div className="dot1"></div>
+        <div className="dot2"></div>
+        <div className="dot3"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Redirect
---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, { useEffect } from 'react'
+
+
+const Page = () => {
+    useEffect(() => {
+        window.location.href = 'https://mateusvergennes-portfolio.com';
+      }, []);
+       
+      return null;
+}
+
+export default Page
